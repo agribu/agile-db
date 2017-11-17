12b8f989662e251f88d3ac0536a378c06567d4ea
agribu <user-agribu-github@gedankenbrut.de>
2017-11-17 13:14:05 +0100

    Adding agile-sdk->idm.group support

--- src/js/agile-sdk-handler/agile-sdk-handler.js
+++ src/js/agile-sdk-handler/agile-sdk-handler.js
@@ -1,3 +1,7 @@
+/**
+ * Documentation available: https://github.com/Agile-IoT/agile-sdk/blob/master/DOCUMENTATION.md
+ */
+
 const fs = require("fs");
 const argv = require('yargs').argv;
 const proxy = require('agile-sdk-proxy');
@@ -6,81 +10,151 @@ const project_dir = "../../../";
 var db_conf = project_dir + "conf/db_conf.json";
 
 function inputHandler() {
-  if (typeof argv.conf === 'string') {
-      db_conf = project_dir + argv.conf;
-      proxy.configure(db_conf);
-  }
 
-  if (argv.createDatabaseTable) {
-      var id, type, database, table;
-      if (typeof argv.id === 'number') { id = argv.id.toString(); }
-      if (typeof argv.type === 'string') { type = argv.type; }
-      if (typeof argv.database === 'string') { database = argv.database; }
-      if (typeof argv.table === 'string') { table = argv.table; }
-      proxy.createDatabaseTable(id, type, database, table);
-  }
+    /**
+     * #######################################
+     *  helper functions
+     * #######################################
+     */
+    if (typeof argv.conf === 'string') {
+        db_conf = project_dir + argv.conf;
+        proxy.configure(db_conf);
+    }
 
-  if (argv.createDatabaseColumn) {
-      var id, type, database, table, column;
-      if (typeof argv.id === 'number') { id = argv.id.toString(); }
-      if (typeof argv.type === 'string') { type = argv.type; }
-      if (typeof argv.database === 'string') { database = argv.database; }
-      if (typeof argv.table === 'string') { table = argv.table; }
-      if (typeof argv.column === 'string') { column = argv.column; }
-      proxy.createDatabaseColumn(id, type, database, table, column);
-  }
+    /**
+     * #######################################
+     *  database functions extends idm.entity
+     * #######################################
+     */
 
-  if (argv.createEntity) {
-      var id, type, name;
-      if (typeof argv.id === 'number') { id = argv.id.toString(); }
-      if (typeof argv.type === 'string') { type = argv.type; }
-      if (typeof argv.name === 'string') { name = argv.name; }
-      proxy.createEntity(id, type, name);
-  }
+    if (argv.createDatabaseTable) {
+        var id, type, database, table;
+        if (typeof argv.id === 'number') { id = argv.id.toString(); }
+        if (typeof argv.type === 'string') { type = argv.type; }
+        if (typeof argv.database === 'string') { database = argv.database; }
+        if (typeof argv.table === 'string') { table = argv.table; }
+        proxy.createDatabaseTable(id, type, database, table);
+    }
 
-  if (argv.deleteEntity) {
-      var id, type;
-      if (typeof argv.id === 'number') { id = argv.id.toString(); }
-      if (typeof argv.type === 'string') { type = argv.type; }
-      proxy.deleteEntity(id, type);
-  }
+    if (argv.createDatabaseColumn) {
+        var id, type, database, table, column;
+        if (typeof argv.id === 'number') { id = argv.id.toString(); }
+        if (typeof argv.type === 'string') { type = argv.type; }
+        if (typeof argv.database === 'string') { database = argv.database; }
+        if (typeof argv.table === 'string') { table = argv.table; }
+        if (typeof argv.column === 'string') { column = argv.column; }
+        proxy.createDatabaseColumn(id, type, database, table, column);
+    }
 
-  if (argv.setEntityAttribute) {
-      var id, type, attr, value;
-      if (typeof argv.id === 'number') { id = argv.id.toString(); }
-      if (typeof argv.type === 'string') { type = argv.type; }
-      if (typeof argv.attr === 'string') { attr = argv.attr; }
-      if (typeof argv.value === 'string') { value = argv.value; }
-      proxy.setEntityAttribute(id, type, attr, value);
-  }
+    /**
+     * #######################################
+     *  idm.entity functions
+     * #######################################
+     */
 
-  if (argv.deleteEntityAttribute) {
-      var id, type, attr;
-      if (typeof argv.id === 'number') { id = argv.id.toString(); }
-      if (typeof argv.type === 'string') { type = argv.type; }
-      if (typeof argv.attr === 'string') { attr = argv.attr; }
-      proxy.deleteEntityAttribute(id, type, attr);
-  }
+    if (argv.createEntity) {
+        var id, type, name;
+        if (typeof argv.id === 'number') { id = argv.id.toString(); }
+        if (typeof argv.type === 'string') { type = argv.type; }
+        if (typeof argv.name === 'string') { name = argv.name; }
+        proxy.createEntity(id, type, name);
+    }
 
-  if (argv.getEntity) {
-      var id, type;
-      if (typeof argv.id === 'number') { id = argv.id.toString(); }
-      if (typeof argv.type === 'string') { type = argv.type; }
-      proxy.getEntity(id, type);
-  }
+    if (argv.deleteEntity) {
+        var id, type;
+        if (typeof argv.id === 'number') { id = argv.id.toString(); }
+        if (typeof argv.type === 'string') { type = argv.type; }
+        proxy.deleteEntity(id, type);
+    }
 
-  if (argv.getEntityByType) {
-      var type;
-      if (typeof argv.type === 'string') { type = argv.type; }
-      proxy.getEntityByType(type);
-  }
+    if (argv.setEntityAttribute) {
+        var id, type, attr, value;
+        if (typeof argv.id === 'number') { id = argv.id.toString(); }
+        if (typeof argv.type === 'string') { type = argv.type; }
+        if (typeof argv.attr === 'string') { attr = argv.attr; }
+        if (typeof argv.value === 'string') { value = argv.value; }
+        proxy.setEntityAttribute(id, type, attr, value);
+    }
+
+    if (argv.deleteEntityAttribute) {
+        var id, type, attr;
+        if (typeof argv.id === 'number') { id = argv.id.toString(); }
+        if (typeof argv.type === 'string') { type = argv.type; }
+        if (typeof argv.attr === 'string') { attr = argv.attr; }
+        proxy.deleteEntityAttribute(id, type, attr);
+    }
+
+    if (argv.getEntity) {
+        var id, type;
+        if (typeof argv.id === 'number') { id = argv.id.toString(); }
+        if (typeof argv.type === 'string') { type = argv.type; }
+        proxy.getEntity(id, type);
+    }
+
+    if (argv.getEntityByType) {
+        var type;
+        if (typeof argv.type === 'string') { type = argv.type; }
+        proxy.getEntityByType(type);
+    }
+
+    /**
+     * #######################################
+     *  idm.group functions
+     * #######################################
+     */
+
+    if (argv.getGroup) {
+        var ownerid, group;
+        if (typeof argv.ownerid === 'string') { ownerid = argv.ownerid; }
+        if (typeof argv.group === 'string') { name = argv.group; }
+        proxy.getGroup(ownerid, group);
+    }
+
+    if (argv.getAllGroups) {
+        proxy.getAllGroups();
+    }
+
+    if (argv.createGroup) {
+        var name;
+        if (typeof argv.name === 'string') { name = argv.name; }
+        proxy.createGroup(name);
+    }
+
+    if (argv.deleteGroup) {
+        var ownerid, name;
+        if (typeof argv.ownerid === 'string') { ownerid = argv.ownerid; }
+        if (typeof argv.name === 'string') { name = argv.name; }
+        proxy.deleteGroup(ownerid, name);
+    }
+
+    if (argv.groupAddEntity) {
+        var ownerid, group, entityid, type;
+        if (typeof argv.ownerid === 'string') { ownerid = argv.ownerid; }
+        if (typeof argv.group === 'string') { group = argv.group; }
+        if (typeof argv.entityid === 'string') { entityid = argv.entityid; }
+        if (typeof argv.type === 'string') { type = argv.type; }
+        proxy.groupAddEntity(ownerid, group, entityid, type);
+    }
+
+    if (argv.groupDelEntity) {
+        var ownerid, group, entityid, type;
+        if (typeof argv.ownerid === 'string') { ownerid = argv.ownerid; }
+        if (typeof argv.group === 'string') { group = argv.group; }
+        if (typeof argv.entityid === 'string') { entityid = argv.entityid; }
+        if (typeof argv.type === 'string') { type = argv.type; }
+        proxy.groupDelEntity(ownerid, group, entityid, type);
+    }
 
 }
 
 inputHandler();
 
 /**
- * Test functions
+ * Test functions: database
+ * node agile-sdk-handler.js --createDatabaseTable --id '0' --type 'db-column' --database 'db_test' --table 'table_test' --column 'column_test'
+ * node agile-sdk-handler.js --createDatabaseColumn --id '0' --type 'db-column' --database 'db_test' --table 'table_test' --column 'column_test'
+ *
+ * Test functions: idm.entity
  * node agile-sdk-handler.js --createEntity --id 0 --type 'database' --name 'db_test' (check first if id is not used)
  * node agile-sdk-handler.js --deleteEntity --id 0 --type 'database'
  * node agile-sdk-handler.js --getEntityByType --type="database"
@@ -88,4 +162,12 @@ inputHandler();
  * node agile-sdk-handler.js --deleteEntityAttribute --id 0 --type 'database' --attr addres
  * node agile-sdk-handler.js --getEntity --id 0 --type 'database'
  * node agile-sdk-handler.js --getEntityByType --type 'database'
+ *
+ * Test functions: idm.group
+ * node agile-sdk-handler.js --getGroup --ownerid 'agile!@!agile-local' --group 'testgroup'
+ * node agile-sdk-handler --getAllGroups
+ * node agile-sdk-handler.js --createGroup --name 'testgroup'
+ * node agile-sdk-handler.js --deleteGroup --ownerid 'agile!@!agile-local' --name 'testgroup'
+ * node agile-sdk-handler.js --groupAddEntity --ownerid 'agile!@!agile-local' --group 'testgroup' --entityid 'bob!@!agile-local' --type 'user'
+ * node agile-sdk-handler.js --groupDelEntity --ownerid 'agile!@!agile-local' --group 'testgroup' --entityid 'bob!@!agile-local' --type 'user'
  */
--- src/js/agile-sdk-proxy/agile-sdk-proxy.js
+++ src/js/agile-sdk-proxy/agile-sdk-proxy.js
@@ -1,11 +1,21 @@
+/**
+ * Documentation available: https://github.com/Agile-IoT/agile-sdk/blob/master/DOCUMENTATION.md
+ */
+
 const fs = require("fs");
 const stringify = require('json-stringify');
 const agile = require('agile-sdk')({
     api: 'http://localhost:8080',
-  	idm: 'http://localhost:3000',
-  	data: 'http://localhost:1338',
-  	token: "cuMQQYdMbrdSUBbWrACYQKrNouRRbWJpIParagC1eELMdZSmZYOTXyVN7mvhnoyz"
+    idm: 'http://localhost:3000',
+    data: 'http://localhost:1338',
+    token: "Pe63N9W3MjnGSkPUH4Uszf6wu3XuzBd2wwtrjCy6oMQoUVk687ggqaV4mJaNPETU"
 });
+
+/**
+ * #######################################
+ *  helper functions
+ * #######################################
+ */
 
 /**
  * @param {Object} obj - json object
@@ -23,61 +33,125 @@ exports.configure = function(db) {
     db_conf = JSON.parse(fs.readFileSync(db));
 }
 
+/**
+ * #######################################
+ *  database functions extends idm.entity
+ * #######################################
+ */
+
 exports.createDatabaseColumn = function(id, type, database, table,  column) {
-		agile.idm.entity.create(id, type, {'database':database, 'table':table, 'column':column}).then(function(result) {
-		  	console.log('Database column created!\n' + pretty(result));
-		});
+    agile.idm.entity.create(id, type, {'database':database, 'table':table, 'column':column}).then(function(result) {
+        console.log('Database column created!\n' + pretty(result));
+    });
 }
 
 exports.createDatabaseTable = function(id, type, database, table) {
-		agile.idm.entity.create(id, type, {'database':database, 'table':table}).then(function(result) {
-		  	console.log('Database table created!\n' + pretty(result));
-		});
+	agile.idm.entity.create(id, type, {'database':database, 'table':table}).then(function(result) {
+        console.log('Database table created!\n' + pretty(result));
+	});
 }
 
+/**
+ * #######################################
+ *  idm.entity functions
+ * #######################################
+ */
+
 exports.createEntity = function(id, type, name) {
-		agile.idm.entity.create(id, type, {'name':name}).then(function(result) {
-		  	console.log('Entity created!\n' + pretty(result));
-		});
+	agile.idm.entity.create(id, type, {'name':name}).then(function(result) {
+        console.log('Entity created!\n' + pretty(result));
+	});
 }
 
 exports.deleteEntity = function(id, type) {
-	  agile.idm.entity.delete(id, type).then(function() {
-	    	console.log('Entity removed!');
-	  });
+    agile.idm.entity.delete(id, type).then(function() {
+        console.log('Entity removed!');
+    });
 }
 
 exports.setEntityAttribute = function(id, type, attr, value) {
-		agile.idm.entity.setAttribute({
+	agile.idm.entity.setAttribute({
         entityId: id,
         entityType: type,
         attributeType: attr,
         attributeValue: value,
     }).then(function(result) {
-				console.log('Entity attribute added!\n' + pretty(result));
-		});
+        console.log('Entity attribute added!\n' + pretty(result));
+	});
 }
 
 exports.deleteEntityAttribute = function(id, type, attr) {
-		agile.idm.entity.deleteAttribute(id, type, attr).then(function(result) {
-		  	console.log('Entity updated!\n' + pretty(result));
-		});
+	agile.idm.entity.deleteAttribute(id, type, attr).then(function(result) {
+        console.log('Entity updated!\n' + pretty(result));
+	});
 }
 
 exports.getEntity = function(id, type) {
-		agile.idm.entity.get(id, type).then(function(result) {
-		  	console.log(pretty(result));
-		});
+	agile.idm.entity.get(id, type).then(function(result) {
+        console.log(pretty(result));
+	});
 }
 
 exports.getEntityByType = function(type) {
-		agile.idm.entity.getByType(type).then(function(entities) {
-		  	console.log(pretty(entities));
-		});
+	agile.idm.entity.getByType(type).then(function(entities) {
+        console.log(pretty(entities));
+	});
 }
 
 exports.getEntitiesSchema = function() {
-		agile.idm.entity.getEntitiesSchema().then(function(jsonschema) {
-		  	console.log('Entities schema:\n' + pretty(jsonschema));
-		});
+	agile.idm.entity.getEntitiesSchema().then(function(jsonschema) {
+        console.log('Entities schema:\n' + pretty(jsonschema));
+	});
+}
+
+/**
+ * #######################################
+ *  idm.group functions
+ * #######################################
+ */
+exports.getGroup = function(ownerid, group) {
+    agile.idm.group.get(ownerid, group).then(function(result) {
+        console.log('Group ' + group + ' owned by ' + ownerid + ':\n' + pretty(result));
+    });
+}
+
+exports.getAllGroups = function() {
+    agile.idm.group.get().then(function(groups) {
+        console.log('List of all groups:\n' + pretty(groups));
+    });
+}
+
+exports.createGroup = function(name) {
+    agile.idm.group.create(name).then(function(group) {
+        console.log('Group created:\n' + pretty(group));
+    });
+}
+
+exports.deleteGroup = function(ownerid, name) {
+    agile.idm.group.delete(ownerid, name).then(function() {
+        console.log('Group removed:\n' + pretty(name));
+    });
+}
+
+exports.groupAddEntity = function(ownerid, group, entityid, type) {
+    console.log('proxy: ' + ownerid + ', ' + group + ', ' + entityid + ', ' + type);
+    agile.idm.group.addEntity({
+        owner: ownerid,
+        name: group,
+        entityId: entityid,
+        entityType: type
+    }).then(function(updated) {
+        console.log('Group updated!\n' + pretty(updated));
+    });
+}
+
+exports.groupDelEntity = function(ownerid, group, entityid, type) {
+    agile.idm.group.removeEntity({
+        owner: ownerid,
+        name: group,
+        entityId: entityid,
+        entityType: type
+    }).then(function(updated) {
+        console.log('Group updated!\n' + pretty(updated));
+    });
 }
