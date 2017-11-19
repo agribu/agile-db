58e9f61c0dd6eecd0e485b352df37224ba200edd
agribu <user-agribu-github@gedankenbrut.de>
2017-11-19 17:10:30 +0100

    added example user and example group operations

---
+++ src/py/agile-u+g.py
@@ -0,0 +1,229 @@
+#!/usr/bin/python3
+# Documentation available: https://github.com/agribu/agile-db/wiki/AGILE-Database-entities
+import os, re, json, argparse
+from utils import mysqlc
+
+# Main configuration file
+main_conf = None
+# Path to agile_conf.json
+agile_conf = None
+# Path to example_groups.json
+example_groups = None
+# Path to example_users.json
+example_users = None
+# Path to agile-sdk-handler
+agile = None
+
+# ##################################### #
+#  main function                        #
+# ##################################### #
+def main():
+    global main_conf, agile_conf, agile, example_users, example_groups
+    parser = argparse.ArgumentParser(description='This python script includes functions to create AGILE user and group entities')
+    parser.add_argument('-c','--config', help='Config file',required=True)
+    parser.add_argument('--createExampleUsers', help='Creates some example users for testing the emergency policy scenario', action='store_true', required=False)
+    parser.add_argument('--deleteExampleUsers', help='Deletes all example users', action='store_true', required=False)
+    parser.add_argument('--createExampleGroups', help='Creates some example groups for testing the emergency policy scenario', action='store_true', required=False)
+    parser.add_argument('--deleteExampleGroups', help='Deletes all example groups', required=False)
+    parser.add_argument('--createMappings', help='Maps example users to example groups', required=False)
+    parser.add_argument('--deleteMappings', help='Deletes all mapping of example users to example groups', required=False)
+    args = parser.parse_args()
+
+    if args.config:
+        with open(args.config) as json_data_file:
+            main_conf = json.load(json_data_file)
+
+        agile_conf = main_conf["agile_conf"]
+        db_conf = main_conf["db_conf"]
+        agile = main_conf["agile-sdk-handler"]
+        example_users = main_conf["example_users"]
+        example_groups = main_conf["example_groups"]
+
+    if args.createExampleUsers:
+        createExampleUsers()
+    if args.deleteExampleUsers:
+        deleteExampleUsers()
+    if args.createExampleGroups:
+        createExampleGroups()
+    if args.deleteExampleGroups:
+        deleteExampleGroups(args.deleteExampleGroups)
+    if args.createMappings:
+        createMappings(args.createMappings)
+    if args.deleteMappings:
+        deleteMappings(args.deleteMappings);
+
+# ##################################### #
+#  helper functions                     #
+# ##################################### #
+def run(cmd):
+    nodejs = "/usr/bin/nodejs "
+    return os.popen(nodejs + cmd).read()
+
+def extractIDs(s):
+    identifiers = []
+    result = re.findall(r'\"id\"\:\s\".+', s)
+    for eid in result:
+        identifiers.append(eid.split('"id": "')[1].split('",')[0])
+    return identifiers
+
+# ##################################### #
+#  user functions                       #
+# ##################################### #
+def addUser(username, authtype, role, password):
+    debug = run(agile
+        + " --conf " + agile_conf
+        + " --createUser"
+        + " --username " + username
+        + " --authtype " + authtype
+        + " --role " + role
+        + " --password" + password)
+    print(debug)
+    return debug
+
+def getUser(username, authtype):
+    debug = run(agile
+        + " --conf " + agile_conf
+        + " --getUser"
+        + " --username " + username
+        + " --authtype " + authtype)
+    # print(debug)
+    return debug
+
+def deleteUser(username, authtype):
+    debug = run(agile
+        + " --conf " + agile_conf
+        + " --deleteUser"
+        + " --username " + username
+        + " --authtype " + authtype)
+    print(debug)
+    return debug
+
+def getCurrentUserInfo():
+    debug = run(agile
+        + " --conf " + agile_conf
+        + " --getCurrentUserInfo")
+    # print(debug)
+    return debug
+
+# ##################################### #
+#  group functions                       #
+# ##################################### #
+def createGroup(name):
+    debug = run(agile
+        + " --conf " + agile_conf
+        + " --createGroup"
+        + " --name " + name)
+    print(debug)
+    return debug
+
+def deleteGroup(ownerid, name):
+    debug = run(agile
+        + " --conf " + agile_conf
+        + " --deleteGroup"
+        + " --ownerid " + ownerid
+        + " --name " + name)
+    print(debug)
+    return debug
+
+def groupAddEntity(ownerid, group, entityid, entitytype):
+    debug = run(agile
+        + " --conf " + agile_conf
+        + " --groupAddEntity"
+        + " --ownerid " + ownerid
+        + " --group " + group
+        + " --entityid " + entityid
+        + " --type " + entitytype)
+    print(debug)
+    return debug
+
+def groupRemoveEntity(ownerid, group, entityid, entitytype):
+    debug = run(agile
+        + " --conf " + agile_conf
+        + " --groupRemoveEntity"
+        + " --ownerid " + ownerid
+        + " --group " + group
+        + " --entityid " + entityid
+        + " --type " + entitytype)
+    print(debug)
+    return debug
+
+# ##################################### #
+#  quick functions                      #
+# ##################################### #
+def createExampleUsers():
+    global example_users
+    with open(example_users) as json_data_file:
+        example_users = json.load(json_data_file)
+
+    for item in example_users.keys():
+        for x in example_users[item]:
+            username = x["user_name"]
+            authtype = x["auth_type"]
+            role = x["role"]
+            password = x["password"]
+            addUser(username, authtype, role, password);
+
+def deleteExampleUsers():
+    global example_users
+    with open(example_users) as json_data_file:
+        example_users = json.load(json_data_file)
+
+    for item in example_users.keys():
+        for x in example_users[item]:
+            username = x["user_name"]
+            authtype = x["auth_type"]
+            deleteUser(username, authtype);
+
+def createExampleGroups():
+    global example_groups
+    with open(example_groups) as json_data_file:
+        example_groups = json.load(json_data_file)
+
+    for name in example_groups:
+        createGroup(name)
+
+def deleteExampleGroups(ownerid):
+    global example_groups
+    with open(example_groups) as json_data_file:
+        example_groups = json.load(json_data_file)
+
+    for name in example_groups:
+        deleteGroup(ownerid, name)
+
+def createMappings(ownerid):
+    global example_groups, example_users
+    with open(example_groups) as json_data_file:
+        example_groups = json.load(json_data_file)
+    with open(example_users) as json_data_file:
+        example_users = json.load(json_data_file)
+
+    for item in example_users.keys():
+            for group in example_groups:
+                if (item == group):
+                    for x in example_users[item]:
+                        user = str(getUser(x["user_name"], x["auth_type"]))
+                        entityids = extractIDs(user)
+                        for eid in entityids:
+                            groupAddEntity(ownerid, group, eid, 'user')
+
+def deleteMappings(ownerid):
+    global example_groups, example_users
+    with open(example_groups) as json_data_file:
+        example_groups = json.load(json_data_file)
+    with open(example_users) as json_data_file:
+        example_users = json.load(json_data_file)
+
+    for item in example_users.keys():
+            for group in example_groups:
+                if (item == group):
+                    for x in example_users[item]:
+                        user = str(getUser(x["user_name"], x["auth_type"]))
+                        entityids = extractIDs(user)
+                        for eid in entityids:
+                            groupRemoveEntity(ownerid, group, eid, 'user')
+
+# ##################################### #
+#  start                                #
+# ##################################### #
+if __name__ == "__main__":
+   main()
