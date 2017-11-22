bf9c4eef5e204f0b9a0c3f6b57383d7120ab28c1
agribu <user-agribu-github@gedankenbrut.de>
2017-11-22 13:17:14 +0100

    changed documentation link

--- src/py/agile-u+g.py
+++ src/py/agile-u+g.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Documentation available: https://github.com/agribu/agile-db/wiki/AGILE-Database-entities
+# Documentation available: https://github.com/agribu/agile-db/wiki/AGILE-Users-and-Groups-(Example)
 import os, re, json, argparse
 from utils import mysqlc
 
