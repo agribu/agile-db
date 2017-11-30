bc11afde6ab8e09ce4ad5e8fe11dc633f4c57127
agribu <user-agribu-github@gedankenbrut.de>
2017-11-30 20:38:52 +0100

    improved install file for initial setup

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 PROJECT_DIR = $('pwd')
 
-git clone https://github.com/Agile-IoT/agile-sdk.git $PROJECT_DIR/src/js/agile-sdk
-cd $PROJECT_DIR/src/js/agile-sdk
-npm install
+echo '#### Installing repository requirements'
+sudo apt-get install -y nodejs npm python3 python3-pip mysql-client
+
+echo '#### Installing ./src/js dependencies'
 cd $PROJECT_DIR/src/js/agile-sdk-proxy
 npm install
 cd $PROJECT_DIR/src/js/agile-sdk-handler
 npm install
+
+echo '#### Installing ./src/py dependencies'
+cd $PROJECT_DIR/src/py
+sudo pip3 install -r requirements.txt
